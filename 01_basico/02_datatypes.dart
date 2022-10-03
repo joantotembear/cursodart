18139e92be42e8249cb55c9abd0462cc50da295d
joantotembear <53118877+joantotembear@users.noreply.github.com>
2022-10-03 10:57:26 -0500

    Prueba2

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,74 @@
+main() {
+// ==== Numeros
+  int a = 10;
+  double b = 5.5;
+  int? c = null;
+
+  int _a = 10;
+  double $b = 40;
+
+  double resultado = a + $b;
+
+  //print(resultado);
+
+// ==== String
+  String nombre = 'Tony';
+  String nombre2 = "Tony";
+  String nombre3 = 'O\'Connor';
+  String apellido = "Stark";
+  String nombreCompleto = '$nombre $apellido';
+  String multilinea = '''
+    Hola Mundo
+    Como estan
+    $nombreCompleto
+  ''';
+
+  //print(nombre);
+  //print(nombre2);
+  //print(nombre3);
+  //print(multilinea);
+
+// ==== Booleanos
+  bool isActive = true;
+  bool isNotActive = !isActive;
+
+  //print(isActive);
+
+// ==== Listas
+  List<String> villanos = ['Lex', 'Red Skull', 'Doom'];
+  List<String> villanos2 = ['lex', 'Thanos']; // los arreglos son base 0
+  villanos.add('Venom');
+  villanos.add('Venom');
+  villanos.add('Venom');
+  villanos.add('Venom');
+  villanos.add('Venom');
+  villanos.add('Venom');
+  //print(villanos);
+  var villanosSet = villanos.toSet();
+  print(villanosSet);
+
+  //=== Set
+  Set<String> villanos3 = {'Lex', 'Red Skull', 'Doom'};
+  villanos3.add('Venom');
+  villanos3.add('Venom');
+  villanos3.add('Venom');
+  villanos3.add('Venom');
+  villanos3.add('Venom');
+  villanos3.add('Venom');
+  villanos3.add('Venom');
+
+  //print(villanos3);
+
+// ==== Mapas  Diccionarios / Objetos
+  Map<String, dynamic> ironman = {
+    'nombre': 'Tony Stark',
+    'poder': 'Inteligencia y dinero',
+    'edad': 45
+  };
+
+  Map<String, dynamic> capitan = new Map();
+  capitan.addAll({'nombre': 'Steve', 'poder': 'Soportar suero sin morir'});
+
+  //print(ironman);
+  //print(capitan);
+}
